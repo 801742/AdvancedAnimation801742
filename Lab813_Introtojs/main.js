f6f34ef3362c0291145819496c9c53784252ecfa
801742 <801742@seq.org>
2021-08-17 13:15:17 -0700

    Intro to JS 8/17

--- Lab813_Introtojs/main.js
+++ Lab813_Introtojs/main.js
@@ -3,7 +3,7 @@
 window.addEventListener("load", init);
 
 // global variables
-var canvas, context, x, y, dx, dy;
+var canvas, context, x, y, dx, dy, x2, y2, dx2, dy2;
 
 function init(){
     // https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement
@@ -11,8 +11,10 @@ function init(){
     // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
     context = canvas.getContext("2d");
 
-    x = y = 100;    // initial x,y canvas location
+    x = y = 500;    // initial x,y canvas location
     dx = dy = 2;    // velocity in x and y directions
+    x2 = y2 = 100;
+    dy2 = dx2 = 4;
 
     animate();      // kick off the animation
 }
@@ -22,6 +24,7 @@ function animate() {
     // erase the HTMLCanvasElement
     context.clearRect(0,0,canvas.width,canvas.height);
     update();   // update location
+    checkEdges();
     draw();     // render
     requestAnimationFrame(animate); // next cycle
 }
@@ -29,7 +32,28 @@ function animate() {
 // move the circle to a new location
 function update() {
     x += dx;    // update x coordinate of location with x velocity
-    y += dy;    // update y coordinate of location with y velocity
+    y += dy;
+    x2 += dx2;
+    y2 += dy2;    // update y coordinate of location with y velocity
+}
+
+function checkEdges(){
+  if(x > canvas.width || x2 > canvas.width){
+    dx = -dx;
+    dx2 = -dx2;
+  }
+  if(y > canvas.height || y2 > canvas.height){
+    dy = -dy;
+    dy2 = -dy2;
+  }
+  if(y < 0 || y2 < 0){
+    dy = -dy;
+    dy2 = -dy2;
+  }
+  if(x < 0 || x2 < 0){
+    dx = -dx;
+    dx2 = -dx2;
+  }
 }
 
 // render a circle
@@ -41,6 +65,16 @@ function draw() {
     context.arc(x, y, radius, 0, 2 * Math.PI);
     context.strokeStyle = "black";  // color to fill
     context.fillStyle = "blue";     // color to stroke
+    context.fill();     // render the fill
+    context.stroke();   // render the stroke
+
+    let radius2 = 8; // local variable radius of the circle
+    // create the circle path
+    context.beginPath();    // clear old path
+    // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc
+    context.arc(x2, y2, radius2, 0, 2 * Math.PI);
+    context.strokeStyle = "black";  // color to fill
+    context.fillStyle = "red";     // color to stroke
     context.fill();     // render the fill
     context.stroke();   // render the stroke
 }
